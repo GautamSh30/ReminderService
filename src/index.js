fcf9f7e36875b78cb36feb7d8b40e4b7c3ec6929
Gautam Sharma <gautam2586272@gmail.com>
2024-05-20 18:22:48 +0530

    added notification cron

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ const { PORT } = require("./config/serverConfig");
 
 const { sendBasicEmail } = require("./services/email-service");
 
-const crom = require("node-cron");
+// const crom = require("node-cron");
+const jobs = require("./utils/job");
+
+const TicketController = require("./controllers/ticket-controller");
 
 const setupAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  app.post("/api/v1/tickets", TicketController.create);
+
   app.listen(PORT, () => {
     console.log(`Server listening to port ${PORT}`);
+    jobs();
 
-    sendBasicEmail(
-      "support@admin.com",
-      "notificationservicesanket@gmail.com",
-      "Testing",
-      "Aur bsdk"
-    );
-
-    crossOriginIsolated.schedule("*/2 * * * *", () => {
-      console.log("running a task every 2 min");
-    });
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "notificationservicesanket@gmail.com",
+    //   "Testing",
+    //   "Aur bsdk"
+    // );
   });
 };
 
