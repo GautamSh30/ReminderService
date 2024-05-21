fd0f95262cf4571890ded2222d87293e89aa9eac
Gautam Sharma <gautam2586272@gmail.com>
2024-05-21 15:58:38 +0530

    emailing using queue

--- src/index.js
+++ src/index.js
@@ -3,18 +3,25 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
-const { createChannel } = require("./utils/messageQueue");
-
 const TicketController = require("./controllers/ticket-controller");
 
 const jobs = require("./utils/job");
+const { subscribeMessage, createChannel } = require("./utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("./config/serverConfig");
+
+const EmailService = require("./services/email-service");
 
 const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
-  // const channel = await createChannel();
+  const channel = await createChannel();
+  subscribeMessage(
+    channel,
+    EmailService.subscribedEvents,
+    REMINDER_BINDING_KEY
+  );
 
   app.post("/api/v1/tickets", TicketController.create);
 
